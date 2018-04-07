cf16842770ee4c670b55bf4a0cca38ce9f866c40
Tejas Khot <tkhot@andrew.cmu.edu>
2018-04-07 16:19:46 -0400

    updated plotting and saving

--- reinforce.py
+++ reinforce.py
@@ -14,7 +14,7 @@ from torch.distributions import Categorical
 
 import matplotlib.pyplot as plt
 
-import ipdb
+# import ipdb
 
 # if gpu is to be used
 use_cuda = torch.cuda.is_available()
@@ -51,9 +51,10 @@ class Reinforce(object):
         self.test_episodes = args.test_episodes
         self.num_steps = args.num_steps
         self.gamma = args.gamma
+        self.expt_name = args.expt_name
+        self.save_path = args.save_path
         self.test_freq = args.test_freq
         self.save_freq = args.save_freq
-        self.expt_name = args.expt_name
         self.train_rewards = []
         self.test_rewards = []
         self.train_steps = []
@@ -91,10 +92,10 @@ class Reinforce(object):
             R = self.gamma * R + (rewards[i] * 1e-2)
             loss = loss - (log_probs[i]*Variable(R))
         loss = loss / len(rewards)
-        self.losses.append(loss)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+        self.losses.append(loss.detach().cpu().numpy())
 
     def train(self, num_episodes):
         print("Going to be training for a total of {} episodes".format(num_episodes))
@@ -104,15 +105,20 @@ class Reinforce(object):
             self.train_rewards.append(sum(rewards))
             self.train_steps.append(steps)
             self.optimize(rewards, log_probs)
+
             if (e+1) % 100 == 0:
                 print("Episode: {}, reward: {}, steps: {}".format(e+1, sum(rewards), steps))
+
             # Freeze the current policy and test over 100 episodes
             if (e+1) % self.test_freq == 0:
                 print("-"*10 + " testing now " + "-"*10)
                 self.test(self.test_episodes)
-            # Save the current policy
+
+            # Save the current policy model
             if (e+1) % (self.save_freq) == 0:
-                torch.save(self.policy.state_dict(), os.path.join(self.save_path, "{}_{}.pkl".format(self.expt_name,e)))
+                torch.save(self.policy.state_dict(), os.path.join(self.save_path, "train_ep_{}.pkl".format(e+1)))
+
+        # plot once when done training
         self.plot_rewards(save=True)
 
     def test(self, num_episodes):
@@ -137,6 +143,7 @@ class Reinforce(object):
     def plot_rewards(self, save=False):
         train_rewards = [self.train_rewards[i:i+self.test_freq] for i in range(0,len(self.train_rewards),self.test_freq)]
         test_rewards = [self.test_rewards[i:i+self.test_episodes] for i in range(0,len(self.test_rewards),self.test_episodes)]
+        train_losses = [self.losses[i:i+self.test_freq] for i in range(0,len(self.losses),self.test_freq)]
 
         # rewards
         train_rewards_mean = [np.mean(i) for i in train_rewards]
@@ -153,6 +160,10 @@ class Reinforce(object):
         train_steps_std = [np.mean(i) for i in train_steps]
         test_steps_std = [np.mean(i) for i in test_steps]
 
+        # loss
+        train_losses_mean = [np.mean(i) for i in train_losses]
+        train_losses_std = [np.std(i) for i in train_losses]
+
         # training : reward over time
         plt.figure(1)
         plt.clf()
@@ -161,7 +172,7 @@ class Reinforce(object):
         plt.ylabel("Avg Reward")
         plt.errorbar(train_nepisodes, train_rewards_mean, yerr=train_rewards_std, color="indigo", uplims=True, lolims=True)
         if save :
-            plt.savefig(self.expt_name + "_train_rewards_{}.png".format(len(self.train_rewards)))
+            plt.savefig(self.expt_name + "train_rewards_{}.png".format(len(self.train_rewards)))
         else:
             plt.show()
             # pause so that the plots are updated
@@ -178,7 +189,7 @@ class Reinforce(object):
         except:
             ipdb.set_trace()
         if save :
-            plt.savefig(self.expt_name + "_test_rewards_{}.png".format(len(self.test_rewards)))
+            plt.savefig(self.expt_name + "test_rewards_{}.png".format(len(self.test_rewards)))
         else:
             plt.show()
 
@@ -190,7 +201,7 @@ class Reinforce(object):
         plt.ylabel("Avg number of steps")
         plt.errorbar(train_nepisodes, train_steps_mean, yerr=train_steps_std, color="navy", uplims=True, lolims=True)
         if save :
-            plt.savefig(self.expt_name + "_train_steps_{}.png".format(len(self.train_steps)))
+            plt.savefig(self.expt_name + "train_steps_{}.png".format(len(self.train_steps)))
         else:
             plt.show()
             # pause so that the plots are updated
@@ -204,39 +215,42 @@ class Reinforce(object):
         plt.ylabel("Avg number of steps")
         plt.errorbar(train_nepisodes, test_steps_mean, yerr=test_steps_std, color="navy", uplims=True, lolims=True)
         if save :
-            plt.savefig(self.expt_name + "_test_steps_{}.png".format(len(self.test_steps)))
+            plt.savefig(self.expt_name + "test_steps_{}.png".format(len(self.test_steps)))
         else:
             plt.show()
 
         # training : avg loss over time
         plt.figure(5)
         plt.clf()
-        plt.title("Avg. Training Loss over {} episodes".format(len(train_nepisodes)))
+        plt.title("Avg. Training Loss over {} episodes".format(train_nepisodes))
         plt.xlabel("Number of training episodes")
         plt.ylabel("Avg. Loss")
-        plt.plot(self.losses, color="crimson")
+        # plt.plot(train_losses_mean, color="crimson")
+        plt.errorbar(train_nepisodes, train_losses_mean, yerr=train_losses_std, color="crimson", uplims=True, lolims=True)
         if save :
-            plt.savefig(self.expt_name + "_train_loss_{}.png".format(len(self.test_rewards)))
+            plt.savefig(self.expt_name + "train_loss_{}.png".format(len(self.test_rewards)))
         else:
             plt.show()
 
 def main():
     parser = argparse.ArgumentParser(description="Using REINFORCE for solving LunarLander")
     parser.add_argument("--gamma", type=float, default=0.99, help="discount factor (default = 0.99)")
-    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate (default = 1e-2)")
-    parser.add_argument("--num_episodes", type=int, default=1000, help="number of episodes")
-    parser.add_argument("--test_episodes", type=int, default=10, help="number of episodes to test on")
+    parser.add_argument("--lr", type=float, default=5e-4, help="learning rate (default = 1e-2)")
+    parser.add_argument("--num_episodes", type=int, default=50000, help="number of episodes")
+    parser.add_argument("--test_episodes", type=int, default=100, help="number of episodes to test on")
     parser.add_argument("--num_steps", type=int, default=50, help="number of steps to run per episode")
     parser.add_argument("--seed", type=int, default=1234, help="random seed")
-    parser.add_argument("--save_freq", type=int, default=1e5, help="checkpoint frequency for saving models")
-    parser.add_argument("--test_freq", type=int, default=200, help="frequency for testing policy")
-    parser.add_argument("--save_path", type=str, default="models", help="path for saving models")
-    parser.add_argument("--expt_name", type=str, default="plots/reinforce1", help="expt name for saving results")
+    parser.add_argument("--save_freq", type=int, default=1e4, help="checkpoint frequency for saving models")
+    parser.add_argument("--test_freq", type=int, default=500, help="frequency for testing policy")
+    parser.add_argument("--save_path", type=str, default="models/reinforce2/", help="path for saving models")
+    parser.add_argument("--expt_name", type=str, default="plots/reinforce2/", help="expt name for saving results")
 
     args = parser.parse_args()
 
     if not os.path.exists(args.save_path):
         os.mkdir(args.save_path)
+    if not os.path.exists(args.expt_name):
+        os.mkdir(args.expt_name)
 
     # create the environment
     env = gym.make("LunarLander-v2")
